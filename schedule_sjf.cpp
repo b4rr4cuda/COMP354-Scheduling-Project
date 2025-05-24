08123147962efe12adbb74dce7c08fca6ca336c1
Sofya <sofya.s.fed@gmail.com>
2025-05-24 03:15:54 +0300

    SJF seems to be working.

--- schedule_sjf.cpp
+++ schedule_sjf.cpp
@@ -5,6 +5,7 @@
 
 #include <iostream>
 #include "schedulers.h"
+#include "cpu.h"
 
 /**
  * @brief Add a task to the list
@@ -12,11 +13,32 @@
  * @param priority Priority of the task
  * @param burst Burst time of the task
  */
-void Scheduler::add(const char *name, const int priority, const int burst){}
+void Scheduler::add(const char *name, const int priority, const int burst) {
+    Task* t = new Task; // freed in schedule ()
+    t->name = strdup(name);
+    t->priority = priority;
+    t->burst = burst;
+
+    // Insert task sorted by ASCENDING time
+
+
+    auto it = tasks.begin();
+    while (it != tasks.end() && (*it)->burst < t->burst) {
+        advance(it,1);
+    }
+    tasks.insert(it, t);
+}
 
 /**
  * @brief Invoke the scheduler
  */
 void Scheduler::schedule() {
     std::cout << "SJF Scheduler running!" << std::endl;
+    while (!tasks.empty()) {
+        Task* t = tasks.front(); // get the first-in task
+        run(t, t->burst);        // SJF: run full burst
+        tasks.pop_front();       // remove from the list
+        free(t->name);
+        delete t;
+    }
 }
