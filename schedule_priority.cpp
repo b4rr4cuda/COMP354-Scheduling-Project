f452db5e34ddac1b16a635e55b49fb8666e21d62
Sofya <sofya.s.fed@gmail.com>
2025-05-24 03:00:17 +0300

    Priority scheduler seems to work.

--- schedule_priority.cpp
+++ schedule_priority.cpp
@@ -5,6 +5,7 @@
 
 #include <iostream>
 #include "schedulers.h"
+#include "cpu.h"
 
 /**
  * @brief Add a task to the list
@@ -12,11 +13,33 @@
  * @param priority Priority of the task
  * @param burst Burst time of the task
  */
-void Scheduler::add(const char *name, const int priority, const int burst) {}
+void Scheduler::add(const char *name, const int priority, const int burst) {
+    Task* t = new Task; // freed in schedule ()
+    t->name = strdup(name);
+    t->priority = priority;
+    t->burst = burst;
+
+    // Insert task sorted by ASCENDING priority
+    // >= so that if equal priority lower name executes first
+    // purely for aesthetic reasons
+    
+    auto it = tasks.begin();
+    while (it != tasks.end() && (*it)->priority >= t->priority) {
+            advance(it,1);
+        }
+    tasks.insert(it, t);
+}
 
 /**
  * @brief Invoke the scheduler
  */
 void Scheduler::schedule() {
     std::cout << "Priority Scheduler running!" << std::endl;
+    while (!tasks.empty()) {
+        Task* t = tasks.front(); // get the first-in task
+        run(t, t->burst);        // Priority: run full burst
+        tasks.pop_front();       // remove from the list
+        free(t->name);
+        delete t;
+    }
 }
